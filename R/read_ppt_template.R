d388a4cb960d78e5e05012c47e11659d1e1d16e7
Esturban <info@evadvisory.ca>
2023-02-23 14:23:40 -0500

    Dependent internal functions
    Creation of the default deck and the required functions for runtime

---
+++ R/read_ppt_template.R
@@ -0,0 +1,21 @@
+#' Read PowerPoint Template
+#'
+#' This function reads in a PowerPoint template file and returns a pptx object to be used for further modifications.
+#'
+#' @param folder_location The folder that hosts the powerpoint template
+#' @param document_name The name of the PowerPoint template file to be read in.
+#'
+#' @return A pptx object for further modifications.
+#' @export
+#'
+#' @importFrom here here
+#' @importFrom officer read_pptx
+#' @examples
+#' \dontrun{
+#' doc <- read_ppt_template("default-eva-template.potx")
+#' }
+#' 
+read_ppt_template <- function(folder_location = "data",document_name = "default-eva-template.potx") {
+  doc <- officer::read_pptx(here::here(folder_location, document_name))
+  return(doc)
+}
